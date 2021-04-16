9be6d27c105a9e245ae91e72429b564037271184
cleberSan <cleberSan@users.noreply.github.com>
2021-04-15 21:12:32 -0300

    modificação campo fornecedores

---
+++ Models/FornecedorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControleEstoque.Models
+{
+    public class FornecedorModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+    }
+}
